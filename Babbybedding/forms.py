6a1b133bcdd7735f94b0c5eb2ea99648f4f2ec1f
tezmahato <tezmahato@gmail.com>
2024-02-02 14:46:16 +0545

    initial project

---
+++ Babbybedding/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class userform(forms.Form):
+    num1=forms.CharField()
+    num2=forms.CharField()
---
+++ Babbybedding/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+from .forms import userform
+
+
+
+
+def aboutUs(request):
+    return render(request,"aboutus.html")
+
+def product(request):
+    return HttpResponse("Welcome to the Product page")
+
+def productDetails(request,productid):
+    return HttpResponse(f"view this product {productid}")
+def homepage(request):
+    data={
+        'title':'BabyBedding'
+    }
+    return render(request,"index.html",data) 
+
+
+def contact(request):
+    form=userform()
+    return render(request,"contact.html",{'form':form})
+
+def userForm(request):
+    form=userform()
+    return render(request,"userform.html",{'form': form})
